95a66d86076069a72f324837c489b632aaef808b
blankcc <894058546@qq.com>
2018-02-01 15:53:24 +0800

    The third edition with some revise

--- calculator3.py
+++ calculator3.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 import sys
 import csv
+INCOME_TAX_QUICK_LOOKUP_TABLE = [
+    IncomeTaxQuickLookupItem(80000, 0.45, 13505),
+    IncomeTaxQuickLookupItem(55000, 0.35, 5505),
+    IncomeTaxQuickLookupItem(35000, 0.30, 2755),
+    IncomeTaxQuickLookupItem(9000, 0.25, 1005),
+    IncomeTaxQuickLookupItem(4500, 0.2, 555),
+    IncomeTaxQuickLookupItem(1500, 0.1, 105),
+    IncomeTaxQuickLookupItem(0, 0.03, 0)
+]
 
 class Args(object):
     def __init__(self):
         self.args = sys.argv[1:]
     def _get_path(self,parameter):
-        if self.args.index(parameter):
+        try:
             index = self.args.index(parameter)
             return self.args[index+1]
-        else:
-            raise ParameterError()
+        except:
+            print("Parameter Error")
+            exit()
     @property
     def config_path(self):
         return self._get_path(self,'-c')
@@ -18,43 +28,43 @@ class Args(object):
     def userdata_path(self):
         return self._get_path(self,'-d')
     @property
-    def income_path(delf):
+    def income_path(self):
         return self._get_path(self,'-o')
 
-args = Args()
 class Config(object):
     def __init__(self):
         self.config = self._read_config()
     def _read_config(self):
         config = {}
-        with open(args.config_path,'r') as f:
+        with open(Args.config_path,'r') as f:
             for line in f.readlines():
                 key,value = line.split('=')
-                if float(value.strip()):
+                try:
                     config[key.strip()]=float(value.strip())
-                else:
-                    raise ParameterError()
+                except:
+                    print("Parametr Error")
+                    exit()
         return config
     def get_config(self,key):
         return self.config[key]
     @property
     def get_JiShuL(self):
-        return self.config[JiShuL]
+        return self.config['JiShuL']
     @property
     def get_JiShuH(self):
-        return self.config[JiShuH]
+        return self.config['JiShuH']
     @property
     def get_total_shebao(self):
-        return (self.config[YangLao]+self.config[YiLiao]+
-            self.config[ShiYe]+self.config[GongShang]+
-            self.config[ShengYu]+self.config[GongJiJin])
+        return (self.config['YangLao']+self.config['YiLiao']+
+            self.config['ShiYe']+self.config['GongShang']+
+            self.config['ShengYu']+self.config['GongJiJin'])
 
 class UserData(object):
     def __init__(self):
         self.userdata = self._read_users_data()
     def _read_users_data(self):
         userdata = []
-        with open(args.userdata_path,'r') as f:
+        with open(Args.userdata_path,'r') as f:
             for line in f.readlines():
                 EId,income = line.strip().split(',')
             try:
@@ -70,44 +80,46 @@ class UserData(object):
 
 class IncomeTaxCalculator(object):
     def calc_for_all_userdata(self):
-    def calc_society_insurance(self):
+        result = []
+        for employee_id, income in self.userdata:
+            data = [employee_id, income]
+            social_insurance_money = '{:.2f}'.format(self.calc_social_insurance_money(income))
+            tax, remain = self.calc_income_tax_and_remain(income)
+            data += [social_insurance_money, tax, remain]
+            result.append(data)
+        return result
+    def calc_society_insurance(income):
+        if income<=Config.get_JiShuL:
+            return Config.get_JiShuL*Config.get_total_shebao
+        elif Config.get_JiShuL<income<Config.get_JiShuH:
+            return income * Config.get_total_shebao
+        else:
+            return Config.get_JiShuH*Config.get_total_shebao
+    def individual_income_tax(self,income):
+        social_insurance_money = self.calc_society_insurance(income)
+        real_income = income - social_insurance_money
+        if real_income <= 0:
+            return  '{:.2f}'.format(0),'{:.2f}'.format(real_income)
+        elif real_income <= 1500:
+            return '{:.2f}'.format(real_income*0.03),'{:.2f}'.format( income - real_income*0.03 - social_insurance_money)
+        elif 1500<real_income<=4500:
+            return '{:.2f}'.format(real_income*0.1-105),'{:.2f}'.format(income - real_income*0.1+105 - social_insurance_money)
+        elif 4500<real_income<=9000:
+            return '{:.2f}'.format(real_income*0.2-555),'{:.2f}'.format(income - real_income*0.2+555 - social_insurance_money)
+        elif 9000<real_income<=35000:
+            return '{:.2f}'.format(real_income*0.25-1005),'{:.2f}'.format(income - real_income*0.25+1005 - social_insurance_money)
+        elif 35000<real_income<=55000:
+            return '{:.2f}'.format(real_income*0.3-2755),'{:.2f}'.format(income - real_income*0.3+2755 - social_insurance_money)
+        elif 55000<real_income<=80000:
+            return '{:.2f}'.format(real_income*0.35-5055),'{:.2f}'.format(income - real_income*0.35+5055 - social_insurance_money)
+        else:
+            return '{:.2f}'.format(real_income*0.45-13505),'{:.2f}'.format(income - real_income*0.45+13505 - social_insurance_money)
+
     def export(self,default='csv'):
         result = self.calc_for_all_userdata()
-        with open("") as f:
+        with open(Args.userdata_path,'w') as f:
             write = csv.writer(f)
             writer.writerows(result)
-def cal(nos,salaries):
-    for salary in salaries:
-        social_security = salary*0.165
-        tax_due = salary - social_security - 3500
-        if tax_due <= 0:
-            final_salaries.append(salary - social_security)
-        elif tax_due <= 1500:
-            final_salaries.append(salary - tax_due*0.03 - social_security)
-        elif 1500<tax_due<=4500:
-            final_salaries.append(salary - tax_due*0.1+105 - social_security)
-        elif 4500<tax_due<=9000:
-            final_salaries.append(salary - tax_due*0.2+555 - social_security)
-        elif 9000<tax_due<=35000:
-            final_salaries.append(salary - tax_due*0.25+1005 - social_security)
-        elif 35000<tax_due<=55000:
-            final_salaries.append(salary - tax_due*0.3+2755 - social_security)
-        elif 55000<tax_due<=80000:
-            final_salaries.append(salary - tax_due*0.35+5055 - social_security)
-        else:
-            final_salaries.append(salary - tax_due*0.45+13505 - social_security)
-    for no in range(len(nos)):
-        print("{}:{:.2f}".format(nos[no],final_salaries[no]))
 
-nos = []
-salaries = []
-final_salaries = []
-try:
-    for arg in sys.argv[1:]:
-        temp = arg.split(":")
-        nos.append(int(temp[0]))
-        salaries.append(int(temp[1]))
-except:
-    print("Parameter Error")
-    exit()
-cal(nos,salaries)
+if __name__ = '__main__':
+
